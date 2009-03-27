44389e4de667e304df1a9e42dbc9976c9da9c971
kdonald <kdonald@f89eb93f-fe00-432c-9d5f-7b86db922d13>
2009-03-27 20:41:27 +0000

    new web flow project template
    git-svn-id: https://src.springframework.org/svn/spring-samples@8 f89eb93f-fe00-432c-9d5f-7b86db922d13

--- templates/webflow/src/main/java/org/springsource/templates/webflow/WelcomeController.java
+++ templates/webflow/src/main/java/org/springsource/templates/webflow/WelcomeController.java
@@ -1,4 +1,4 @@
-package org.springsource.samples.webflow.gettingstarted;
+package org.springsource.templates.webflow;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
