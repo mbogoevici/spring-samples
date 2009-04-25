030699cf38ba3c365fbbff6c5d3237efa0aeaa1f
kdonald <kdonald@f89eb93f-fe00-432c-9d5f-7b86db922d13>
2009-04-25 04:55:35 +0000

    myapp
    git-svn-id: https://src.springframework.org/svn/spring-samples@69 f89eb93f-fe00-432c-9d5f-7b86db922d13

--- templates/mvc/trunk/template/src/main/java/mytld/mycompany/myapp/WelcomeController.java
+++ templates/mvc/trunk/template/src/main/java/mytld/mycompany/myapp/WelcomeController.java
@@ -1,4 +1,4 @@
-package org.springframework.templates.mvc;
+package mytld.mycompany.myapp;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
