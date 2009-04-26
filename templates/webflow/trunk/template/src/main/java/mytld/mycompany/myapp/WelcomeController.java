2388368b18fa524515c54de61ef63004cbf9093f
kdonald <kdonald@f89eb93f-fe00-432c-9d5f-7b86db922d13>
2009-04-26 23:46:49 +0000

    webflow template
    git-svn-id: https://src.springframework.org/svn/spring-samples@81 f89eb93f-fe00-432c-9d5f-7b86db922d13

---
+++ templates/webflow/trunk/template/src/main/java/mytld/mycompany/myapp/WelcomeController.java
@@ -0,0 +1,20 @@
+package mytld.mycompany.myapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application welcome page.
+ */
+@Controller
+public class WelcomeController {
+
+	/**
+	 * Simply selects the welcome view to render by returning void and relying
+	 * on the default request-to-view-translator.
+	 */
+	@RequestMapping(method = RequestMethod.GET)
+	public void welcome() {
+	}
+}
