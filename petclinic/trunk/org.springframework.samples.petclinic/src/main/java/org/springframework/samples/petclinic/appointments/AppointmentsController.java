847c21545b403210c1e4f9f4e4dad04aa2ba4629
kdonald <kdonald@f89eb93f-fe00-432c-9d5f-7b86db922d13>
2009-05-27 22:01:55 +0000

    experiments
    git-svn-id: https://src.springframework.org/svn/spring-samples@109 f89eb93f-fe00-432c-9d5f-7b86db922d13

--- petclinic/trunk/org.springframework.samples.petclinic/src/main/java/org/springframework/samples/petclinic/appointments/AppointmentsController.java
+++ petclinic/trunk/org.springframework.samples.petclinic/src/main/java/org/springframework/samples/petclinic/appointments/AppointmentsController.java
@@ -5,7 +5,6 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.petclinic.util.ExternalContext;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
