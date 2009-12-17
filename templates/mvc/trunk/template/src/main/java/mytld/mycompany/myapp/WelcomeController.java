551d33c06e71eef6290ed0e357a1774896e36f98
kdonald <kdonald@f89eb93f-fe00-432c-9d5f-7b86db922d13>
2009-12-17 22:20:07 +0000

    polish
    git-svn-id: https://src.springframework.org/svn/spring-samples@221 f89eb93f-fe00-432c-9d5f-7b86db922d13

--- templates/mvc/trunk/template/src/main/java/mytld/mycompany/myapp/WelcomeController.java
+++ templates/mvc/trunk/template/src/main/java/mytld/mycompany/myapp/WelcomeController.java
@@ -1,5 +1,6 @@
 package mytld.mycompany.myapp;
 
+import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,11 +12,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/welcome")
 public class WelcomeController {
 
+	private Logger logger = org.slf4j.LoggerFactory.getLogger(WelcomeController.class);
+
 	/**
 	 * Simply selects the welcome view to render by returning void and relying
 	 * on the default request-to-view-translator.
 	 */
 	@RequestMapping(method = RequestMethod.GET)
 	public void welcome() {
+		logger.info("Welcome!");
 	}
 }
---
+++ templates/mvc/trunk/template/src/test/java/mytld/mycompany/myapp/WelcomeControllerTests.java
@@ -0,0 +1,14 @@
+package mytld.mycompany.myapp;
+
+import org.junit.Test;
+
+public class WelcomeControllerTests {
+	
+	private WelcomeController controller = new WelcomeController();
+	
+	@Test
+	public void welcome() {
+		controller.welcome();
+	}
+	
+}
