c040404ad92733637bf2e53b5f025b1031a4732c
kdonald <kdonald@f89eb93f-fe00-432c-9d5f-7b86db922d13>
2010-03-17 10:19:12 +0000

    polish
    git-svn-id: https://src.springframework.org/svn/spring-samples@411 f89eb93f-fe00-432c-9d5f-7b86db922d13

--- petcare/trunk/src/main/java/org/springframework/samples/petcare/appointments/messaging/DoctorMailTransformer.java
+++ petcare/trunk/src/main/java/org/springframework/samples/petcare/appointments/messaging/DoctorMailTransformer.java
@@ -8,6 +8,7 @@ import org.springframework.integration.annotation.Transformer;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.mail.MailMessage;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.samples.petcare.appointments.messaging.AppointmentMessage.MessageType;
 import org.springframework.samples.petcare.util.templating.StringTemplate;
 import org.springframework.samples.petcare.util.templating.StringTemplateFactory;
 
@@ -15,8 +16,10 @@ public class DoctorMailTransformer {
 	
 	private StringTemplateFactory templateFactory;
 	
-	private Resource mailTemplateResource = new ClassPathResource("/templates/new-appointment-mail.st");
-	
+	private Resource newTemplate = new ClassPathResource("/templates/new-appointment-mail.st");
+
+	private Resource canceledTemplate = new ClassPathResource("/templates/canceled-appointment-mail.st");
+
 	@Inject
 	public DoctorMailTransformer(JdbcTemplate jdbcTemplate, StringTemplateFactory templateFactory) {
 		this.templateFactory = templateFactory;
@@ -27,10 +30,16 @@ public class DoctorMailTransformer {
 		SimpleMailMessage mailMessage = new SimpleMailMessage();
 		mailMessage.setFrom("noreply@petcare.com");
 		mailMessage.setTo("name@emailAddress.com");
-		mailMessage.setSubject("New appointment added");
-		StringTemplate mailTemplate = templateFactory.getStringTemplate(mailTemplateResource);
-		mailTemplate.setAttribute("appointment", message.getAppointment());
-		mailMessage.setText(mailTemplate.toString());
+		StringTemplate textTemplate;
+		if (message.getType().equals(MessageType.APPOINTMENT_ADDED)) {
+			mailMessage.setSubject("New appointment added");
+			textTemplate = templateFactory.getStringTemplate(newTemplate);
+		} else {
+			mailMessage.setSubject("Appointment cancelled");			
+			textTemplate = templateFactory.getStringTemplate(canceledTemplate);
+		}
+		textTemplate.setAttribute("appointment", message.getAppointment());
+		mailMessage.setText(textTemplate.toString());
 		return mailMessage;
 	}
 	
