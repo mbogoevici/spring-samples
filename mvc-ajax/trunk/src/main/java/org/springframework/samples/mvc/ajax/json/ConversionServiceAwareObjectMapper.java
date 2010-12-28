f492bc530448f2fa5631c053e61787c0040eb248
kdonald <kdonald@f89eb93f-fe00-432c-9d5f-7b86db922d13>
2010-12-28 00:04:22 +0000

    upgrade to latest
    git-svn-id: https://src.springframework.org/svn/spring-samples@605 f89eb93f-fe00-432c-9d5f-7b86db922d13

--- mvc-ajax/trunk/src/main/java/org/springframework/samples/mvc/ajax/json/ConversionServiceAwareObjectMapper.java
+++ mvc-ajax/trunk/src/main/java/org/springframework/samples/mvc/ajax/json/ConversionServiceAwareObjectMapper.java
@@ -4,7 +4,7 @@ import org.codehaus.jackson.map.AnnotationIntrospector;
 import org.codehaus.jackson.map.DeserializationConfig;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.SerializationConfig;
-import org.codehaus.jackson.map.introspect.VisibilityChecker.Std;
+import org.codehaus.jackson.map.introspect.VisibilityChecker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.ConversionService;
 
@@ -17,8 +17,8 @@ public class ConversionServiceAwareObjectMapper extends ObjectMapper {
 	@Autowired
 	public ConversionServiceAwareObjectMapper(ConversionService conversionService) {
 		AnnotationIntrospector introspector = AnnotationIntrospector.pair(new FormatAnnotationIntrospector(conversionService), DEFAULT_ANNOTATION_INTROSPECTOR);
-		SerializationConfig sconfig = new SerializationConfig(DEFAULT_INTROSPECTOR, introspector, Std.defaultInstance());
-		DeserializationConfig dconfig = new DeserializationConfig(DEFAULT_INTROSPECTOR, introspector, Std.defaultInstance());
+		SerializationConfig sconfig = new SerializationConfig(DEFAULT_INTROSPECTOR, introspector,  VisibilityChecker.Std.defaultInstance(), null);
+		DeserializationConfig dconfig = new DeserializationConfig(DEFAULT_INTROSPECTOR, introspector,  VisibilityChecker.Std.defaultInstance(), null);
 		setSerializationConfig(sconfig);
 		setDeserializationConfig(dconfig);
 	}
