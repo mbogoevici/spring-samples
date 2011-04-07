e82b8b7d1d0237830b13f47b5ddb34dd750f20b4
rstoyanchev <rstoyanchev@f89eb93f-fe00-432c-9d5f-7b86db922d13>
2011-04-07 19:40:26 +0000

    upgrade to Spring 3.1 build snapshots
    git-svn-id: https://src.springframework.org/svn/spring-samples@611 f89eb93f-fe00-432c-9d5f-7b86db922d13

--- mvc-showcase/src/main/java/org/springframework/samples/mvc/data/custom/CustomArgumentController.java
+++ mvc-showcase/src/main/java/org/springframework/samples/mvc/data/custom/CustomArgumentController.java
@@ -2,38 +2,15 @@ package org.springframework.samples.mvc.data.custom;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.support.WebArgumentResolver;
-import org.springframework.web.context.request.NativeWebRequest;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.annotation.AnnotationMethodHandlerAdapter;
 
 @Controller
 public class CustomArgumentController {
 
-	// request related
-	
-	@Autowired
-	public CustomArgumentController(AnnotationMethodHandlerAdapter controllerInvoker) {
-		// register a custom resolver for @RequestAttribute arguments
-		controllerInvoker.setCustomArgumentResolver(new WebArgumentResolver() {
-			public Object resolveArgument(MethodParameter param, NativeWebRequest request) throws Exception {
-				RequestAttribute attr = param.getParameterAnnotation(RequestAttribute.class);
-				if (attr != null) {
-					return request.getAttribute(attr.value(), WebRequest.SCOPE_REQUEST);
-				} else {
-					return WebArgumentResolver.UNRESOLVED;
-				}
-			}
-		});
-	}
-	
 	@ModelAttribute
 	void beforeInvokingHandlerMethod(HttpServletRequest request) {
 		request.setAttribute("foo", "bar");
