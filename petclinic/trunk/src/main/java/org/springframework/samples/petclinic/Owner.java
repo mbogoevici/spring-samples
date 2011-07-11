2fd23150f29c63a67c72f85515e93c2068e55654
Marius Bogoevici <marius.bogoevici@gmail.com>
2011-07-11 12:14:04 -0400

    Remove cascading, as deleting a Pet will try to remove its owner as well.

--- petclinic/trunk/src/main/java/org/springframework/samples/petclinic/Owner.java
+++ petclinic/trunk/src/main/java/org/springframework/samples/petclinic/Owner.java
@@ -74,6 +74,10 @@ public class Owner extends Person {
 		pet.setOwner(this);
 	}
 
+    public void removePet(Pet pet) {
+		getPetsInternal().remove(pet);
+    }
+
 	/**
 	 * Return the Pet with the given name, or null if none found for this Owner.
 	 *
